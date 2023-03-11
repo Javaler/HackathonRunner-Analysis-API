007037ad9a2c59dd031f549c73a4ab6489824968
Utricularor <mulberry.kzuki@gmail.com>
2023-03-12 00:16:56 +0900

    modified database info

--- dbconnect.py
+++ dbconnect.py
@@ -1,17 +1,18 @@
 import psycopg2
-import os
+import key
 
 def DBConnect():
     print('DBConnect')
-    conn = psycopg2.connect(
-            host=os.environ.get('HOST'),
-            user=os.environ.get('USER'),
-            password=os.environ.get('PASSWORD'),
-            database=os.environ.get('DATABASE')
-        )
+    conn = psycopg2.connect(user=key.user,
+                            password=key.password,
+                            host=key.host,
+                            dbname=key.dbname,
+                            sslmode=key.sslmode)
     
     cur = conn.cursor()
     cur.execute('SELECT * FROM post;')
     data = cur.fetchall()
+    # 接続を閉じる
+    conn.close()
 
     return data
